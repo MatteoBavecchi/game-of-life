a1b0fd05b3aadd7d188de32d941140f7cc5a76d3
matteobavecchi <matteo.bavecchi@stud.unifi.it>
2022-06-13 18:47:25 +0200

    Added redux-toolkit and a redux example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Flex, Slide } from '@chakra-ui/react';
+import { RootState } from './Redux/Store/Store';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './Redux/Features/Game';
 
 function App() {
+  const count = useSelector((state: RootState) => state.game.value)
+  const dispatch = useDispatch()
   return (
-    <div className="App">
+    <Flex
+      w="100%"
+      h="100vh"
+      justifyContent="center"
+    >
       <Heading>Hello world</Heading>
-    </div>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+
+    </Flex>
   );
 }
 
